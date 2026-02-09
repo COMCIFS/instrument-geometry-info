34a1d058a778797eb808399847a2b474a79f7f49
Thomas Kluyver <thomas.kluyver@xfel.eu>
2026-02-09 15:26:42 +0000

    More experimenting with streamlit

--- Tools/streamlit-eg.py
+++ Tools/streamlit-eg.py
@@ -1,20 +1,108 @@
+import hashlib
 import io
+import shutil
+import tempfile
+from numbers import Real
+from pathlib import Path
+from time import perf_counter
 
 import streamlit as st
 import numpy as np
+import requests
 from dxtbx.model.experiment_list import ExperimentListFactory
 
-from dials2imgcif import guess_file_type, make_cif, ArchiveUrl
+from dials2imgcif import guess_archive_type, guess_file_type, make_cif, ArchiveUrl
+
+DATA_DIR = Path("/gpfs/exfel/data/scratch/kluyvert/imgcif-source-data")
+ARCHIVE_EXTS = {'ZIP': '.zip', 'TGZ': '.tar.gz', 'TBZ': '.tar.bz2', 'TXZ': '.tar.xz'}
+SIZE_LIMIT = 5 * (1024 ** 3)
+BYTES_UNITS = ['B', 'KiB', 'MiB', 'GiB', 'TiB']
+
+def fmt_bytes(n: Real) -> str:
+    n = float(n)
+    for suffix in BYTES_UNITS:
+        if n < 1024:
+            break
+        n /= 1024
+
+    return f'{n:.1f} {suffix}'
+
+
+def download_and_unpack(archive_url, ext):
+    hashed_url = hashlib.sha3_256(archive_url.encode('utf-8')).hexdigest()
+    dir_for_url = DATA_DIR / hashed_url
+    print(f"{dir_for_url=}")
+    if dir_for_url.is_dir():
+        contents = list(dir_for_url.iterdir())
+        print("Found something", contents)
+        if contents:
+            return dir_for_url
+
+    resp = requests.get(archive_url, stream=True)
+    if (size := int(resp.headers.get('content-length', '0'))) > SIZE_LIMIT:
+        raise RuntimeError(f"Download too large ({fmt_bytes(size)})")
+
+    with tempfile.NamedTemporaryFile(suffix=ext) as tf:
+        download_progress = st.progress(0, f'Downloading {fmt_bytes(size)}')
+        bytes_read = 0
+        t0 = perf_counter()
+        for chunk in resp.iter_content(chunk_size=4 * 1024 * 1024):
+            bytes_read += len(chunk)
+            if bytes_read > size:
+                raise RuntimeError("Server lied about download size")
+            tf.write(chunk)
+            rate = bytes_read / (perf_counter() - t0)
+            msg = f'Downloaded {fmt_bytes(bytes_read)} / {fmt_bytes(size)} ({fmt_bytes(rate)}/s)'
+            download_progress.progress(bytes_read / size, msg)
+        download_progress.empty()
+
+        unpacked = Path(tempfile.mkdtemp(dir=DATA_DIR))
+        # TODO: this should be tougher against archives doing weird things
+        try:
+            print("Unpacking to", unpacked)
+            with st.spinner("Unpacking"):
+                shutil.unpack_archive(tf.name, unpacked)
+            print("Renaming to", dir_for_url)
+            unpacked.rename(dir_for_url)
+        except:
+            shutil.rmtree(unpacked)
+            raise
+
+        return dir_for_url
+
 
 print("Running script")
 
 st.title("ImgCIF creator")
 
-expt_path = st.text_input("EXPT file", None)
-if expt_path is None:
+url = st.text_input("Download URL")
+
+archive_fmt_guess = guess_archive_type(url)
+archive_type = st.pills(
+    "File format", ["ZIP", "TGZ", "TBZ", "TXZ"], default=archive_fmt_guess
+)
+
+if not url or not archive_type:
     st.stop()
 
-expts = ExperimentListFactory.from_json_file(expt_path)
+unpacked_archive = download_and_unpack(url, ARCHIVE_EXTS[archive_type])
+st.text(unpacked_archive)
+
+unpacked_archive = Path("/gpfs/exfel/data/scratch/kluyvert/imgcif-conv/6RLR/cbf/")
+
+path_in_archive = st.text_input("Data path in archive")
+if not path_in_archive:
+    st.stop()
+
+#expt_path = st.text_input("EXPT file", None)
+#if expt_path is None:
+#    st.stop()
+
+@st.cache_data(show_spinner="Reading files...")
+def load_expt_list(path):
+    return ExperimentListFactory.from_filenames([path])
+
+expts = load_expt_list(unpacked_archive / path_in_archive)
 
 st.write(f"Found {len(expts)} experiment(s) with "
          f"{sum(len(e.imageset) for e in expts)} total images.\n")
@@ -37,7 +125,28 @@ download_info = [
         "TBZ"
     )
 ]
+download_info = [
+    ArchiveUrl(
+        "",
+        "/gpfs/exfel/data/scratch/kluyvert/imgcif-conv/6RLR/cbf/",
+        "TBZ"
+    )
+]
 
+sio = io.StringIO()
+make_cif(
+    expts,
+    sio,
+    data_name="generated",
+    locations=download_info,
+    doi=doi,
+    file_type=file_type,
+    overload_value=None,
+)
+st.download_button("Download CIF file", sio.getvalue(), file_name="generated.cif", on_click="ignore", icon=":material/download:")
+
+st.header("ImgCIF preview")
+st.text("Showing a subset of the data")
 sio = io.StringIO()
 sio.write("# ImgCIF preview - incomplete data\n\n")
 make_cif(
@@ -52,15 +161,3 @@ make_cif(
 )
 
 st.code(sio.getvalue(), language=None)
-
-sio = io.StringIO()
-make_cif(
-    expts,
-    sio,
-    data_name="generated",
-    locations=download_info,
-    doi=doi,
-    file_type=file_type,
-    overload_value=None,
-)
-st.download_button("Download CIF file", sio.getvalue(), file_name="generated.cif", on_click="ignore", icon=":material/download:")
